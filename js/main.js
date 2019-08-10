ea2a01875f2c9268d216e46fac64d9f2e34cadc3
Sling-J <nurel32@gmail.com>
2019-08-10 13:03:24 +0600

    add (attendance / turnstiles)

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
    const usersTable = document.getElementById('users-table');
-   
+
    selectHandle(usersTable);
 });
 
@@ -8,10 +8,10 @@ function selectHandle(container) {
    const selectAllCb = container.querySelector('#select-all-cb');
    const allCheckbox = container.querySelectorAll('.users-table__checkbox');
 
-   selectAllCb.addEventListener('click', event => 
-      allCheckbox.forEach(item => 
+   selectAllCb.addEventListener('click', event =>
+      allCheckbox.forEach(item =>
          event.target.checked ?
-            item.checked = true :
-            item.checked = false
-   ))
+         item.checked = true :
+         item.checked = false
+      ))
 }
